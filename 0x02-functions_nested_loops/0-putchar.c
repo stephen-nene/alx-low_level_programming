ee70e1dac2190b6196ebd1ce54fc8a221213bba3
+stephen.nene <+>
2023-03-05 16:11:44 +0300

    neer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,14 @@
 
 int main(void)
 {
-    char c = '_';
-    write(1, &c, 1);
-    c = 'p';
-    write(1, &c, 1);
-    c = 'u';
-    write(1, &c, 1);
-    c = 't';
-    write(1, &c, 1);
-    c = 'c';
-    write(1, &c, 1);
-    c = 'h';
-    write(1, &c, 1);
-    c = 'a';
-    write(1, &c, 1);
-    c = 'r';
-    write(1, &c, 1);
-    c = '\n';
-    write(1, &c, 1);
-    return (0);
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
