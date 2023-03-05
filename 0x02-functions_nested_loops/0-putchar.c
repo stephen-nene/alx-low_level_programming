63e61871254d2acfc4ba01373ef70c6670b775af
+stephen.nene <+>
2023-03-05 15:11:27 +0300

    file one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main-print putchar followed by a new line
+ *
+ *Return :always 0
+ */
+  int main(void)
+ {
+      putchar('_' );
+      putchar ('p' );
+      putchar ('u' );
+      putchar ('t' );
+      putchar ('c' );
+      putchar ('h' );
+      putchar ('a' );
+      putchar ('r' );
+      putchar ('\n');
+
+       return 0 ;
+}
+
+
+
+
+
