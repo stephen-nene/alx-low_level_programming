380530bf448d68b52ba50b3267911d7f5b23a9b7
stephen nene <stevekid705@gmail.com>
2023-06-09 08:11:19 +0300

    chess thing

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,34 @@
-#include "main.h"
+// #include "main.h"
 
-/**
- * print_chessboard - prints the chessboard
- * @a: pointer to pieces to print
- *
- * Return: void
- */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
+// /**
+//  * print_chessboard - prints the chessboard
+//  * @a: pointer to pieces to print
+//  *
+//  * Return: void
+//  */
+// void print_chessboard(char (*a)[8])
+// {
+// 	int i, j;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+// 	for (i = 0; i < 8; i++)
+// 	{
+// 		for (j = 0; j < 8; j++)
+// 		{
+// 			_putchar(a[i][j]);
+// 		}
+// 		_putchar('\n');
+// 	}
+// }
+
+#include <stdio.h>
+
+void print_chessboard(char (*a)[8]) {
+    int row, col;
+
+    for (row = 0; row < 8; row++) {
+        for (col = 0; col < 8; col++) {
+            printf("%c", a[row][col]);
+        }
+        printf("\n");
+    }
 }
