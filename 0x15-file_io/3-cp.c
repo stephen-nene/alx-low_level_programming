227a12817bafc628e4acadf49ca6af2136d907b6
stephen nene <stevekid705@gmail.com>
2023-07-13 08:24:13 +0300

    done with file inputs

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,130 @@
+#include "main.h"
+
+int write_to_file(char *buff, int to_fd, int read_chars, char *f_to);
+int read_files(int from_fd, int to_fd, char *f_from, char *f_to);
+int open_files(char *f_from, char *f_to);
+
+/**
+ * main - Entry point
+ * 
+ */
+
+int main(int argc, char **argv)
+
+{
+	char *f_from;
+	char *f_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f_from = argv[1];
+	f_to = argv[2];
+
+	open_files(f_from, f_to);
+	exit(0);
+	return (0);
+}
+
+/**
+ * 
+ *
+ */
+
+int open_files(char *f_from, char *f_to)
+
+{
+	int from_fd;
+	int to_fd;
+
+	from_fd = open(f_from, O_RDONLY);
+	if (from_fd == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", f_from);
+		exit(98);
+	}
+	to_fd = open(f_to, O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (to_fd == -1)
+	{
+		/*if (errno == EEXIST) was removed*/
+		to_fd = open(f_to, O_WRONLY | O_TRUNC);
+		if (to_fd == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", f_to);
+			exit(99);
+		}
+	}
+	read_files(from_fd, to_fd, f_from, f_to);
+	if (close(from_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+		exit(100);
+	}
+	if (close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+ * 
+ *
+ */
+
+int read_files(int from_fd, int to_fd, char *f_from, char *f_to)
+
+{
+	int read_chars;
+	char buff[1024];
+
+	read_chars = read(from_fd, buff, 1024);
+	if (read_chars == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", f_from);
+		exit(98);
+	}
+	write_to_file(buff, to_fd, read_chars, f_to);
+	while (read_chars != 0)
+	{
+		read_chars = read(from_fd, buff, 1024);
+		if (read_chars == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", f_from);
+			exit(98);
+		}
+		if (read_chars == 0)
+			return (0);
+		write_to_file(buff, to_fd, read_chars, f_to);
+	}
+	return (0);
+}
+
+/**
+ * 
+ *
+ */
+
+int write_to_file(char *buff, int to_fd, int read_chars, char *f_to)
+
+{
+	int i;
+
+	for (i = 0; i < read_chars; i++)
+	{
+		if (write(to_fd, &buff[i], 1) == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", f_to);
+			exit(99);
+		}
+	}
+	return (0);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,44 @@
+# 0x15. C - File I/O
+<!-- ----------- -->
+
+## C Syscall
+
+# Technologies used
+
+  ![Vim](https://img.shields.io/badge/VIM-%2311AB00.svg?&style=for-the-badge&logo=vim&logoColor=white)  ![EMACs](https://img.shields.io/badge/Emacs-%237F5AB6.svg?&style=for-the-badge&logo=gnu-emacs&logoColor=white) ![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)
+   ![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+  ![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white)  ![C](https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white)
+   ![Stack Overflow](https://img.shields.io/badge/-Stackoverflow-FE7A16?style=for-the-badge&logo=stack-overflow&logoColor=white)  ![twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)
+
+
+
+
+## About
+An introductory project on:
+- How to create, open, close, read and write files
+- File descriptors
+- System calls
+- The difference between a function and a system call
+
+
+## File Descriptions
+* 0-read_textfile.c - function that reads a text file and prints it to the POSIX standard output.
+
+* 1-create_file.c - function that creates a file.
+
+* 2-append_text_to_file.c - function that appends text at the end of a file.
+
+* 3-cp.c - function that copies the content of one file to another file.
+
+* 4. elf - program that displays the information contained in the elf header at the start of the elf file.
+
+# Contributor
+ - [ ![BuiltBy](https://img.shields.io/badge/Built-By-GE7A10?style=flat-square&logo=BuzzFeed&logoColor=white)](https://github.com/stephen-nene)
+ **[stevo-nene](https://github.com/stephen-nene)**
+
+
+
+---------
+## Copyright
+ - **NeneCorp** **<span>&copy; 2023</span>**
+---------
